379d7275fb3df02aa3a3439a8496ca87097a6aeb
Lorenzo <lorenzo-a@live.fr>
2023-11-26 22:26:19 +0100

    For loop

--- cleanHello/CleanWorld.java
+++ cleanHello/CleanWorld.java
@@ -8,6 +8,13 @@ public class CleanWorld {
     /** Le programme commence ici */
     public static void main(String[] args) {
         sayHelloTo("world");
+
+        for(int i= 0; i <= 150; i++){
+            sayHelloTo(Integer.toString(i));
+        }
+
+        arrayLoop();
+
     }
 
     /** affiche le message "hello" au destinataire fourni
@@ -16,6 +23,13 @@ public class CleanWorld {
      */
     private static void sayHelloTo(String recipient) {
         System.out.println("Hello " + recipient);
+    }
+
+    private static void arrayLoop() {
+        int[] myArray = new int[]{7,2,4};
+        for (int i=0; i<myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
     }
 
 }
